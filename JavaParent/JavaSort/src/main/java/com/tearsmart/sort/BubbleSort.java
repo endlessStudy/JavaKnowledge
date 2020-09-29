f3fd8b2dbb93f14042b0286c2776fd8ed8044789
tear-smart <461274926@qq.com>
2020-09-29 17:12:45 +0800

    添加leetCode项目

---
+++ JavaParent/JavaSort/src/main/java/com/tearsmart/sort/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.tearsmart.sort;
+
+/**
+ * @author 刘彦磊
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 5, 8};
+        sort(arr);
+    }
+
+    private static void sort(int[] arr) {
+        int length = arr.length;
+        for (int i = length - 1; i > 0; i--) {
+            
+
+        }
+    }
+}
